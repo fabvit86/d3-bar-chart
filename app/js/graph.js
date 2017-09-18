b5cc05dfc5a45436cd9a9ca38656ddba76d41802
Fabrizio Vitelli <fabvit86@yahoo.it>
2017-09-18 11:43:48 +0200

    added better labels and improved overall look

--- app/js/graph.js
+++ app/js/graph.js
@@ -14,7 +14,7 @@ $.getJSON(url, (json, textStatus) => {
 	const firstDate = new Date(data[0][0]),
 	 			lastDate  = new Date(data[numberOfElements-1][0])
 
-	const margins = {top: 20, right: 20, bottom: 30, left: 120}
+	const margins = {top: 20, right: 20, bottom: 60, left: 120}
 	const chartHeight = 600 - margins.top - margins.bottom
 	const chartWidth = 1000 - margins.left - margins.right
 	const barWidth = chartWidth / numberOfElements
@@ -45,11 +45,9 @@ $.getJSON(url, (json, textStatus) => {
 						    .data(dataObject)
 						  .enter().append("g")
 						    .attr("transform", (d, i) => "translate(" + i * barWidth + ", 0)" ) // translate the g element horizontally
-						    // .text().attr("dx", "1em")
 
   bar.append("rect") // insert a rect in the g element
   		.attr("class", "bar")
-  		// .attr("x", (d) => x(new Date(d.date)))
   		.attr("y", (d) => y(d.gross)) // y coordinate of the rect (ex: if y height is 10px, y must be set to chartHeight-10)
 	    .attr("width", barWidth - 1) // width of the rect, leave 1px for bars' spacing
 	    .attr("height", (d) => chartHeight - y(d.gross)) // height of the rect
@@ -58,7 +56,6 @@ $.getJSON(url, (json, textStatus) => {
 	    	tooltip.transition()
 	    		.duration(300)
 	    		.style("opacity", 1) // show the tooltip
-	    		console.log(d3.select('.tooltip').node().height)
 	    	tooltip.html(formatTime(new Date(d.date)) + "<hr><span class='gross'>" + formatCurrency(d.gross) + " Billions</span>")
          .style("left", (d3.event.pageX - d3.select('.tooltip').node().offsetWidth - 5) + "px")
          .style("top", (d3.event.pageY - d3.select('.tooltip').node().offsetHeight) + "px");
@@ -71,34 +68,40 @@ $.getJSON(url, (json, textStatus) => {
 	    	tooltip.html("")
 	    })
 
-	// svg.append("g")
- //    .attr("class", "y axis")
- //    .call(yAxis)
- //  .append("text")
- //    .attr("transform", "rotate(-90)")
- //    .attr("y", 6)
- //    .attr("dy", ".71em")
- //    .style("text-anchor", "end")
- //    .text("Frequency");
 	//x axis line:
 	const xAxis = svgchart.append('g')
-									.attr("class", "x-axis")
+									.classed("x-axis", true)
 							    .attr("transform", "translate(0," + chartHeight + ")") // put the g on the bottom
 							    .call(d3.axisBottom(x)) // call d3.axisBottom(x) on the g, to generate the axis within the g
   // x axis label:							    
 	xAxis.append("text")
-		.attr("class", "axisLabel")
+		.classed("axisLabel", true)
 		.text("Date")
-		.attr("dy", "1em") // y offset
+		.attr("dx", "20em") // x offset
+		.attr("dy", "2.5em") // y offset
+	xAxis.append("text")
+		.classed("smallText", true)
+		.text("[quarter's year and month]")
+		.attr("dx", "44.5em")
+		.attr("dy", "4.3em")
   xAxis.selectAll("text").style("text-anchor", "middle") // center x axis ticks' text
 	
 	//y axis line:
-	svgchart.append('g')
-		.attr("class", "y-axis")
-	 	.call(d3.axisLeft(y))
-	 	.append("text")
-	 		.attr("class", "axisLabel")
-	 		.text("Gross")
-	 		.attr("dx", ".8em") // x offset
-	 		.attr("dy", "-0.25em")
+	const yAxis = svgchart.append('g')
+									.classed("y-axis", true)
+								 	.call(d3.axisLeft(y))
+  // y axis label:
+	yAxis.append("text")
+ 		.attr("id", "yAxisLabel")
+ 		.classed("axisLabel", true)
+ 		.text("Gross")
+ 		.attr("dx", "-15em") // x offset
+ 		.attr("dy", "-3.25em") // y offset
+ 		.attr("transform", "rotate(-90)") // rotate the label vertically
+ 	yAxis.append("text")
+ 		.classed("smallText", true)
+		.text(" [billions of $]")
+		.attr("dx", "-20.7em") // x offset
+		.attr("dy", "-5.9em") // y offset
+		.attr("transform", "rotate(-90)")
 })
